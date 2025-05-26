707fc9bc352468311fe4c12dfd954f68dc30f78b
EduParize <eduardoparize.05@gmail.com>
2025-05-26 19:57:31 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,210 +1,179 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
-
 /*
-Lista 6.1
 Autor: Eduardo Parize
 RA: 25075923
+LISTA: 10
 */
-
 int main()
 {
 //Exercicio 1:
 /*
-float num1, num2, num3;
-int opcao;
-float geometrica, ponderada, harmonica, aritmetica;
+    int i, valores[6];
+    for(i=0;i<=5;i++){
+        printf("Insira o valor para o %d inteiro: \n", i+1);
+        scanf("%d", &valores[i]);
+    }
 
-printf("Insira o valor do primeiro numero: \n");
-scanf("%f", &num1);
-printf("Insira o valor do segundo numero: \n");
-scanf("%f", &num2);
-printf("Insira o valor do terceiro numero: \n");
-scanf("%f", &num3);
-
-printf("------------\n1- Media geometrica\n2- Media ponderada\n3- Media harmonica\n4- Media aritmetica\n\nEscolha uma opcao:\n------------\n");
-scanf("%d", &opcao);
-
-switch(opcao){
-case 1:
-    geometrica = cbrt(num1*num2*num3);
-    printf("%.2f", geometrica);
-    break;
-case 2:
-    ponderada= (num1+2*num2+3*num3)/6;
-    printf("%.2f", ponderada);
-    break;
-case 3:
-    harmonica= 1/((1/num1)+(1/num2)+(1/num3));
-    printf("%.2f", harmonica);
-    break;
-case 4:
-    aritmetica= (num1+num2+num3)/3;
-    printf("%.2f", aritmetica);
-    break;
-}
+    for(i=0;i<=5;i++){
+        printf("%d ", valores[i]);
+    }
 */
 //Exercicio 2:
 /*
-int i;
-printf("Insira um numero: \n");
-scanf("%d", &i);
+int i, valores[6];
+    for(i=0;i<=5;i++){
+        printf("Insira o valor para o %d inteiro: \n", i+1);
+        scanf("%d", &valores[i]);
+    }
 
-switch(i){
-case 1:
-    printf("Domingo\n");
-    break;
-case 2:
-    printf("Segunda-feira\n");
-    break;
-case 3:
-    printf("Terca-feira\n");
-    break;
-case 4:
-    printf("Quarta-feira\n");
-    break;
-case 5:
-    printf("Quinta-feira\n");
-    break;
-case 6:
-    printf("Sexta-feira\n");
-    break;
-case 7:
-    printf("Sabado\n");
-    break;
-default:
-    printf("Numero invalido");
-    break;
-}
+    for(i=5;i>=0;i--){
+        printf("%d ", valores[i]);
+    }
 */
 //Exercicio 3:
 /*
-int opcao;
-float i, j, resultado;
-printf("---------------");
-printf("Escolha uma opcao:\n");
-printf("1- Soma\n");
-printf("2- Substracao\n");
-printf("3- Multiplicacao\n");
-printf("4- Divisao\n");
-printf("---------------\n");
-scanf("%d", &opcao);
+int i, valores[5];
+float soma=0;
+    for(i=0;i<=4;i++){
+        printf("Insira o valor para o %d inteiro: \n", i+1);
+        scanf("%d", &valores[i]);
+        soma= soma+valores[i];
 
-printf("Insira o valor do primeiro numero: \n");
-scanf("%f", &i);
-printf("Insira o valor do segundo numero: \n");
-scanf("%f", &j);
+    }
 
-switch(opcao){
-case 1:
-    resultado= i+j;
-    printf("Resultado: %.2f\n", resultado);
-    break;
-case 2:
-    resultado= i-j;
-    printf("Resultado: %.2f\n", resultado);
-    break;
-case 3:
-    resultado= i*j;
-    printf("Resultado: %.2f\n", resultado);
-    break;
-case 4:
-    resultado= i/j;
-    printf("Resultado: %.2f\n", resultado);
-    break;
-default:
-    printf("Insira um numero valido:\n");
-    break;
-}
+    for(i=0;i<=4;i++){
+        printf("%d ", valores[i]);
+    }
+    printf("\n%.2f", soma/5);
 */
 //Exercicio 4:
 /*
-float valor, preco_final;
-char estado;
+    int i, A[6]={1,0,5,-2,-5,7};
+    int soma1=0;
+    soma1 = A[0]+A[1]+A[5];
+    printf("soma das posicoes 0, 1 e 5: %d\n",soma1 );
 
-printf("Insira o valor do produto:\n");
-scanf("%f", &valor);
-fflush(stdin);
+    A[4]=100;
 
-printf("------------\n");
-printf("MG - m\n");
-printf("SP - s\n");
-printf("RJ - r\n");
-printf("MS - n\n");
-printf("------------\n");
-printf("Insira estado:\n");
-scanf("%c", &estado);
-
-switch(estado){
-case 'm':
-    preco_final= valor*1.07;
-    break;
-case 's':
-    preco_final= valor*1.12;
-    break;
-case 'r':
-    preco_final= valor*1.15;
-    break;
-case 'n':
-    preco_final= valor*1.08;
-    break;
-default:
-    printf("Erro!\n");
-    break;
-}
-printf("Valor final: %.2f", preco_final);
+    for(i=0;i<=5;i++){
+        printf("%d\n",A[i]);
+    }
 */
-//Exercicio 5:
+//Exercicio 6:
 /*
-int i;
-printf("Insira um numero: \n");
-scanf("%d", &i);
+int i,pares=0, vet[10];
+printf("Insira dez valores: \n");
 
-switch(i){
-case 1:
-    printf("Janeiro\n");
-    break;
-case 2:
-    printf("Fevereiro\n");
-    break;
-case 3:
-    printf("Marco\n");
-    break;
-case 4:
-    printf("Abril\n");
-    break;
-case 5:
-    printf("Maio\n");
-    break;
-case 6:
-    printf("Junho\n");
-    break;
-case 7:
-    printf("Julho\n");
-    break;
-case 8:
-    printf("Agosto\n");
-    break;
-case 9:
-    printf("Setembro\n");
-    break;
-case 10:
-    printf("Outubro\n");
-    break;
-case 11:
-    printf("Novembro\n");
-    break;
-case 12:
-    printf("Dezembro\n");
-    break;
-default:
-    printf("Numero invalido!\n");
+for(i=0;i<=9;i++){
+    scanf("%d", &vet[i]);
+    if(vet[i]%2==0){
+        pares++;
+    }
+}
+printf("Numero de pares: %d\n", pares);
+*/
+//Exercicio 7:
+/*
+int i, X[10], maior, menor;
+printf("Insira 10 valores: \n");
+scanf("%d", &X[0]);
+maior = X[0];
+
+scanf("%d", &X[1]);
+if(X[1]>X[0]){
+maior = X[1];
+menor = X[0];}else{
+                maior=X[0];
+                menor=X[1];
+}
+
+for(i=2;i<=9;i++){
+    scanf("%d", &X[i]);
+    if(X[i]>maior){
+       maior=X[i];
+    }
+    if(X[i]<menor){
+        menor=X[i];
+    }
+}
+printf("Maior: %d\n", maior);
+printf("Menor: %d\n", menor);
+*/
+//Exercicio 8:
+/*
+float vet[10], soma;
+int i, negativos=0;
+
+printf("Insira 10 valores: \n");
+
+for(i=0;i<=9;i++){
+    scanf("%f", &vet[i]);
+    if(vet[i]<0){
+        negativos++;
+        }else{
+            soma=soma+vet[i];
+            }
+}
+printf("Numero de negativos: %d\n", negativos);
+printf("Soma dos positivos: %.2f\n", soma);
+*/
+//Exercicio 9:
+/*
+int i, A[10],B[10],C[10];
+printf("Insira 10 valores para o array A: \n");
+for(i=0;i<=9;i++){
+    scanf("%d", &A[i]);
+}
+printf("Insira 10 valores para o array B: \n");
+for(i=0;i<=9;i++){
+    scanf("%d", &B[i]);
+}
+
+for(i=0;i<=9;i++){
+    C[i]=A[i]-B[i];
+    printf("%d ", C[i]);
 }
 */
+//Exercicio 10:
+/*
+int vet[100],i, nao_multiplos=1;
+
+for(i=0;i<=99;){
+        if(nao_multiplos%7==0){
+            nao_multiplos++;
+            continue;
+        }else{
+    vet[i]=nao_multiplos;
+    i++;
+    }
+    nao_multiplos++;
 }
+for(i=0;i<=99;i++){
+    printf("%d ", vet[i]);
+}
+*/
+//Exercicio 11:
+/*
+float A[20], B[20];
+int i;
 
+printf("Insira 20 valores para o array A: \n");
 
-
-
+for(i=0;i<=19;i++){
+    scanf("%f", &A[i]);
+    B[i]=sqrt(A[i]);
+}
+printf("Array A: \n");
+for(i=0;i<=19;i++){
+    printf("%.2f ", A[i] );
+}
+printf("\n");
+printf("Array B: \n");
+for(i=0;i<=19;i++){
+    printf("%.2f ", B[i] );
+}
+*/
+    return 0;
+}
